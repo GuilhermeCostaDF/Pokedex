c4d328d60ef2cca2e90213d30295004838b36467
GuidoCostaDF <guidocostadf@gmail.com>
2022-10-26 13:05:53 -0300

    efetuando a primeira requisição

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const offset = 10;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?${offset}=0&limit=${limit}`;
+
+fetch(url)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.error(error);
+  })
+  .finally(function () {
+    console.log("Requisição conluída");
+  });
