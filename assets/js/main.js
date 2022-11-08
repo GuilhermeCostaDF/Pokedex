204c103d00b44d9ae33eeacd201df71871bd5049
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-08 09:45:38 -0300

    separando o consumo da api da manupilação html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-const offset = 10;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?${offset}=0&limit=${limit}`;
+const pokemonList = document.getElementById("pokemonList");
 
 function convertPokemonToLi(pokemon) {
   return `
@@ -23,15 +21,10 @@ function convertPokemonToLi(pokemon) {
   `;
 }
 
-const pokemonList = document.getElementById("pokemonList");
-
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((pokemons) => {
-    for (let i = 0; i < pokemons.length; i++) {
-      const pokemon = pokemons[i];
-      pokemonList.innerHTML += convertPokemonToLi(pokemon);
-    }
-  })
-  .catch((error) => console.error(error));
+pokeApi.getPokemons().then((pokemons) => {
+  const listItem = [];
+  for (let i = 0; i < pokemons.length; i++) {
+    const pokemon = pokemons[i];
+    listItem.push(pokemon);
+  }
+});
