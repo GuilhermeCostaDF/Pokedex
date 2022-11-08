44e2cd5752948f76ab71063c270b92cb6d34a3bc
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-08 09:58:04 -0300

    utilizando map para diminuir a verbosidade

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,10 +21,6 @@ function convertPokemonToLi(pokemon) {
   `;
 }
 
-pokeApi.getPokemons().then((pokemons) => {
-  const listItem = [];
-  for (let i = 0; i < pokemons.length; i++) {
-    const pokemon = pokemons[i];
-    listItem.push(pokemon);
-  }
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
 });
