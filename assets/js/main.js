f557ea235697bb731e1512d40b498dd2a759274d
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-08 09:12:14 -0300

    manipulando o resultado da requisiçao

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,11 @@ const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?${offset}=0&limit=${limit}`;
 
 fetch(url)
-  .then(function (response) {
-    response.json().then(function (responseBody) {
-      console.log(responseBody);
-    });
-  })
+  .then((response) => response.json())
+  .then((jsonBody) => console.log(jsonBody))
   .catch(function (error) {
     console.error(error);
   })
   .finally(function () {
     console.log("Requisição conluída");
   });
-
-//teste
