a7eb32d855c3d45d1d33aa577a35fc5d57b13d6e
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-08 10:39:50 -0300

    manipulando multiplas requisiçoes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,28 @@
 const pokemonList = document.getElementById("pokemonList");
 
+function convertPokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes
+    .map((typeSlot) => {
+      return `
+    <li class="type">${typeSlot.type.name}</li>
+    `;
+    })
+    .join("");
+}
+
 function convertPokemonToLi(pokemon) {
   return `
     <li class="pokemon">
-      <span class="number">#001</span>
+      <span class="number">#${pokemon.order}</span>
       <span class="name">${
         pokemon.name[0].toUpperCase() + pokemon.name.substring(1)
       }</span>
       <div class="detail">
         <ol class="types">
-          <li class="type">grass</li>
-          <li class="type">poison</li>
+          ${convertPokemonTypesToLi(pokemon.types)}
         </ol>
         <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+          src="${pokemon.sprites.other.dream_world.front_default}"
           alt="${pokemon.name}"
         />
       </div>
